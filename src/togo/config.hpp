0b4e370f7df63e00ef9f617cef30456c850adffc
Coranna Howard <me@komiga.com>
2014-06-28 00:37:28 -0400

    config: added platform and architecture detection definitions.

--- src/togo/config.hpp
+++ src/togo/config.hpp
@@ -53,9 +53,71 @@ namespace togo {
 	#define TOGO_USE_CONSTRAINTS
 #else
 	#if !defined(TOGO_DEBUG) && (defined(DEBUG) || !defined(NDEBUG))
-		#define TOGO_DEBUG 1
+		#define TOGO_DEBUG
 	#endif
-#endif
+#endif // defined(DOXYGEN_CONSISTS_SOLELY_OF_UNICORNS_AND_CONFETTI)
+
+/** @name System detection */ /// @{
+
+#if defined(DOXYGEN_CONSISTS_SOLELY_OF_UNICORNS_AND_CONFETTI)
+	/**
+		Defined if the target platform is Linux.
+	*/
+	#define TOGO_PLATFORM_LINUX
+	/**
+		Defined if the target platform is MacOS.
+	*/
+	#define TOGO_PLATFORM_MACOS
+	/**
+		Defined if the target platform is Windows.
+	*/
+	#define TOGO_PLATFORM_WINDOWS
+
+	/**
+		Defined if the target platform is POSIX-compliant.
+
+		This is defined if TOGO_PLATFORM_LINUX or TOGO_PLATFORM_MACOS
+		are defined.
+	*/
+	#define TOGO_PLATFORM_IS_POSIX
+
+	/**
+		Defined if the target processor is x86.
+	*/
+	#define TOGO_ARCH_X86
+	/**
+		Defined if the target processor is x86_64.
+	*/
+	#define TOGO_ARCH_X86_64
+#else
+	#if defined(__linux__)
+		#define TOGO_PLATFORM_LINUX
+	#elif defined(macintosh) || defined(Macintosh) || \\
+		(defined(__APPLE__) && defined(__MACH__))
+
+		#define TOGO_PLATFORM_MACOS
+	#elif defined(_WIN32)
+		#define TOGO_PLATFORM_WINDOWS
+	#else
+		#error "unrecognized platform"
+	#endif
+
+	#if defined(TOGO_PLATFORM_LINUX) || defined(TOGO_PLATFORM_MACOS)
+		#define TOGO_PLATFORM_IS_POSIX
+	#endif
+
+	#if defined(__amd64__) || defined(__x86_64__) || \
+		defined(_M_AMD64) || defined(_M_X64)
+
+		#define TOGO_ARCH_X86_64
+	#elif defined(__i386__) || defined(_M_IX86)
+		#define TOGO_ARCH_X86
+	#else
+		#error "unrecognized processor architecture"
+	#endif
+#endif // defined(DOXYGEN_CONSISTS_SOLELY_OF_UNICORNS_AND_CONFETTI)
+
+/// @} // end of name-group System detection
 
 /** @} */ // end of doc-group config
 
