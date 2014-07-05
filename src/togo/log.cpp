37fda0287036306b44808de0ff57bc2c3737d862
Coranna Howard <me@komiga.com>
2014-07-05 00:49:43 -0400

    log: moved format to macros; added traced variants; made error traced.

--- src/togo/log.cpp
+++ src/togo/log.cpp
@@ -16,56 +16,14 @@
 namespace togo {
 
 namespace {
-
-static char const* const
-_log_prefix[]{
-	"",
-	"debug: ",
-	"error: ",
-};
-
-static Mutex _log_mutex{};
-
-inline static void
-vprintf(
-	log::Type const l,
-	char const* msg,
-	va_list va
-) {
-	if (l != log::Type::general) {
-		std::printf("%s", _log_prefix[static_cast<unsigned>(l)]);
-	}
-	std::vprintf(msg, va);
-}
-
+	static Mutex _log_mutex{};
 } // anonymous namespace
 
-void
-log::printf(
-	log::Type const l,
-	char const* msg,
-	...
-) {
-	MutexLock log_mutex_lock{_log_mutex};
+void log::printf(char const* const msg, ...) {
+	MutexLock l{_log_mutex};
 	va_list va;
 	va_start(va, msg);
-	togo::vprintf(l, msg, va);
-	va_end(va);
-}
-
-void
-log::printf_trace(
-	log::Type const l,
-	unsigned line,
-	char const* file,
-	char const* msg,
-	...
-) {
-	MutexLock log_mutex_lock{_log_mutex};
-	std::printf("%s @ %4d: ", file, line);
-	va_list va;
-	va_start(va, msg);
-	togo::vprintf(l, msg, va);
+	std::vprintf(msg, va);
 	va_end(va);
 }
 
--- src/togo/log.hpp
+++ src/togo/log.hpp
@@ -20,59 +20,42 @@ namespace log {
 	@{
 */
 
-/// Log type.
-enum class Type : unsigned {
-	general = 0,
-	debug,
-	error,
-};
-
 #if defined(TOGO_COMPILER_CLANG) || \
 	defined(TOGO_COMPILER_GCC)
-	__attribute__((__format__ (__printf__, 2, 3)))
+	__attribute__((__format__ (__printf__, 1, 2)))
 #endif
-void
-printf(
-	log::Type const l,
-	char const* msg,
-	...
-);
-
-#if defined(TOGO_COMPILER_CLANG) || \
-	defined(TOGO_COMPILER_GCC)
-	__attribute__((__format__ (__printf__, 4, 5)))
-#endif
-void
-printf_trace(
-	log::Type const l,
-	unsigned line,
-	char const* file,
-	char const* msg,
-	...
-);
+void printf(char const* const msg, ...);
 
 /// Log message.
 #define TOGO_LOG(msg) \
-	::togo::log::printf(::togo::log::Type::general, msg)
+	::togo::log::printf(msg)
 
 /// Log formatted message.
 #define TOGO_LOGF(msg, ...) \
-	::togo::log::printf(::togo::log::Type::general, msg, __VA_ARGS__)
+	::togo::log::printf(msg, __VA_ARGS__)
+
+/// Log message (traced).
+#define TOGO_LOG_TRACED(msg) \
+	TOGO_LOGF("%s @ %4d: " msg, __FILE__, __LINE__)
+
+/// Log formatted message (traced).
+#define TOGO_LOGF_TRACED(msg, ...) \
+	TOGO_LOGF("%s @ %4d: " msg, __FILE__, __LINE__, __VA_ARGS__)
 
 /// Log error message.
 #define TOGO_LOG_ERROR(msg) \
-	::togo::log::printf(::togo::log::Type::error, msg)
+	TOGO_LOG_TRACED("error: " msg)
 
 /// Log formatted error message.
 #define TOGO_LOG_ERRORF(msg, ...) \
-	::togo::log::printf(::togo::log::Type::error, msg, __VA_ARGS__)
+	TOGO_LOGF_TRACED("error: " msg, __VA_ARGS__)
 
 #if defined(TOGO_DEBUG)
 	#define TOGO_LOG_DEBUG(msg) \
-		::togo::log::printf_trace(::togo::log::Type::debug, __LINE__, __FILE__, msg)
+		TOGO_LOG_TRACED("debug: " msg)
 
 	#define TOGO_LOG_DEBUGF(msg, ...) \
-		::togo::log::printf_trace(::togo::log::Type::debug, __LINE__, __FILE__, msg, __VA_ARGS__)
+		TOGO_LOGF_TRACED("debug: " msg, __VA_ARGS__)
 #else
 	/// Log debug message.
 	#define TOGO_LOG_DEBUG(msg) (void(0))
