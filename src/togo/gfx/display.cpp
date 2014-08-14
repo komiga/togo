c3e541efb0075980415708f82d499c05411dfd3c
Coranna Howard <me@komiga.com>
2014-08-13 18:59:34 -0700

    gfx/display: moved input_buffer private func implementations to .cpp.

--- src/togo/gfx/display.cpp
+++ src/togo/gfx/display.cpp
@@ -4,8 +4,10 @@
 */
 
 #include <togo/config.hpp>
+#include <togo/gfx/display.hpp>
 #include <togo/impl/gfx/types.hpp>
 #include <togo/impl/gfx/display/private.hpp>
+#include <togo/assert.hpp>
 #include <togo/input_buffer.hpp>
 
 #if (TOGO_CONFIG_GRAPHICS_BACKEND == TOGO_GRAPHICS_BACKEND_SDL)
@@ -29,6 +31,30 @@ unsigned display::width(gfx::Display const* display) {
 
 unsigned display::height(gfx::Display const* display) {
 	return display->_height;
+}
+
+// private
+
+bool display::is_attached_to_input_buffer(gfx::Display const* display) {
+	return display->_input_buffer;
+}
+
+void display::attach_to_input_buffer(gfx::Display* display, InputBuffer& ib) {
+	TOGO_ASSERT(
+		!gfx::display::is_attached_to_input_buffer(display),
+		"display is already attached to an input buffer"
+	);
+	display->_input_buffer = &ib;
+	attach_to_input_buffer_impl(display);
+}
+
+void display::detach_from_input_buffer(gfx::Display* display) {
+	TOGO_ASSERT(
+		gfx::display::is_attached_to_input_buffer(display),
+		"display is not attached to an input buffer"
+	);
+	detach_from_input_buffer_impl(display);
+	display->_input_buffer = nullptr;
 }
 
 } // namespace gfx
--- src/togo/impl/gfx/display/private.hpp
+++ src/togo/impl/gfx/display/private.hpp
@@ -6,8 +6,6 @@
 #pragma once
 
 #include <togo/config.hpp>
-#include <togo/utility.hpp>
-#include <togo/assert.hpp>
 #include <togo/gfx/types.hpp>
 #include <togo/impl/gfx/types.hpp>
 #include <togo/input_types.hpp>
@@ -16,33 +14,13 @@ namespace togo {
 namespace gfx {
 namespace display {
 
-inline bool is_attached_to_input_buffer(
-	gfx::Display* const display
-) {
-	return display->_input_buffer;
-}
+bool is_attached_to_input_buffer(gfx::Display const* display);
+void attach_to_input_buffer(gfx::Display* display, InputBuffer& ib);
+void detach_from_input_buffer(gfx::Display* display);
 
+// Backend
 void attach_to_input_buffer_impl(gfx::Display* display);
 void detach_from_input_buffer_impl(gfx::Display* display);
-
-inline void attach_to_input_buffer(gfx::Display* display, InputBuffer& ib) {
-	TOGO_ASSERT(
-		!gfx::display::is_attached_to_input_buffer(display),
-		"display is already attached to an input buffer"
-	);
-	display->_input_buffer = &ib;
-	attach_to_input_buffer_impl(display);
-}
-
-inline void detach_from_input_buffer(gfx::Display* display) {
-	TOGO_ASSERT(
-		gfx::display::is_attached_to_input_buffer(display),
-		"display is not attached to an input buffer"
-	);
-	detach_from_input_buffer_impl(display);
-	display->_input_buffer = nullptr;
-}
-
 void process_events(InputBuffer& ib);
 
 } // namespace display
