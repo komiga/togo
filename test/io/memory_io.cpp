423765fab9b0d1bb99b70473238444c3cce6c5ed
Coranna Howard <me@komiga.com>
2014-08-17 03:38:03 -0700

    test/io/memory: added MemoryReader test.

--- test/io/memory_io.cpp
+++ test/io/memory_io.cpp
@@ -1,7 +1,8 @@
 
 #include <togo/assert.hpp>
-#include <togo/io.hpp>
 #include <togo/memory.hpp>
+#include <togo/io.hpp>
+#include <togo/array.hpp>
 #include <togo/memory_io.hpp>
 
 #include "../common/helpers.hpp"
@@ -17,5 +18,8 @@ main() {
 	test_writer(stream, true);
 	TOGO_ASSERTE(io::seek_to(stream, 0) == 0);
 	test_reader(stream, true);
+
+	MemoryReader stream_const{array::begin(stream.buffer()), array::size(stream.buffer())};
+	test_reader(stream_const, true);
 	return 0;
 }
