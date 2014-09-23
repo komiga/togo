8cdb33734d9cf9240e3de73c89ecd71ce9c54692
Coranna Howard <me@komiga.com>
2014-09-23 02:34:03 -0700

    hash: added calc_generic<H>(), rebased calc32() & calc64(); tidy.

--- src/togo/hash.hpp
+++ src/togo/hash.hpp
@@ -27,17 +27,6 @@ namespace hash {
 // TODO: Build constexpr version of MurmurHash2 (both lengths) and
 // use it instead of FNV-1a
 
-namespace {
-	static constexpr am::hash::HashLength const
-	hash32_length = am::hash::HashLength::HL32,
-	hash64_length = am::hash::HashLength::HL64;
-} // anonymous namespace
-
-/** @cond INTERNAL */
-TOGO_CONSTRAIN_SAME(hash32, am::detail::hash::fnv_hash_type<hash32_length>);
-TOGO_CONSTRAIN_SAME(hash64, am::detail::hash::fnv_hash_type<hash64_length>);
-/** @endcond */
-
 /// 32-bit hash identity (hash of nothing).
 static constexpr hash32 const
 IDENTITY32{0};
@@ -46,48 +35,94 @@ IDENTITY32{0};
 static constexpr hash64 const
 IDENTITY64{0};
 
+namespace {
+	static constexpr am::hash::HashLength const
+	hash32_length = am::hash::HashLength::HL32,
+	hash64_length = am::hash::HashLength::HL64;
+
+	template<class H>
+	struct hash_type_length;
+
+	template<> struct hash_type_length<hash32> {
+		static constexpr auto const value = hash32_length;
+	};
+
+	template<> struct hash_type_length<hash64> {
+		static constexpr auto const value = hash64_length;
+	};
+
+	template<class H>
+	struct hash_identity;
+
+	template<> struct hash_identity<hash32> {
+		static constexpr auto const value = hash::IDENTITY32;
+	};
+
+	template<> struct hash_identity<hash64> {
+		static constexpr auto const value = hash::IDENTITY64;
+	};
+} // anonymous namespace
+
+/** @cond INTERNAL */
+TOGO_CONSTRAIN_SAME(hash32, am::detail::hash::fnv_hash_type<hash32_length>);
+TOGO_CONSTRAIN_SAME(hash64, am::detail::hash::fnv_hash_type<hash64_length>);
+/** @endcond */
+
 /**
-	Calculate 32-bit hash.
+	Calculate H-bit hash.
 */
-inline hash32
-calc32(
+template<class H>
+inline H calc_generic(
 	char const* const data,
 	unsigned const size
 ) {
 	return
 		size != 0
-		? am::hash::fnv1a<hash32_length>(data, size)
-		: hash::IDENTITY32
+		? am::hash::fnv1a<hash_type_length<H>::value>(data, size)
+		: hash_identity<H>::value
 	;
+}
+
+/**
+	Calculate H-bit hash from string reference.
+*/
+template<class H>
+inline H calc_generic(StringRef const& ref) {
+	return hash::calc_generic<H>(ref.data, ref.size);
+}
+
+/**
+	Calculate 32-bit hash.
+*/
+inline hash32 calc32(
+	char const* const data,
+	unsigned const size
+) {
+	return hash::calc_generic<hash32>(data, size);
 }
 
 /**
 	Calculate 32-bit hash from string reference.
 */
 inline hash32 calc32(StringRef const& ref) {
-	return hash::calc32(ref.data, ref.size);
+	return hash::calc_generic<hash32>(ref.data, ref.size);
 }
 
 /**
 	Calculate 64-bit hash.
 */
-inline hash64
-calc64(
+inline hash64 calc64(
 	char const* const data,
 	unsigned const size
 ) {
-	return
-		size != 0
-		? am::hash::fnv1a<hash64_length>(data, size)
-		: hash::IDENTITY64
-	;
+	return hash::calc_generic<hash64>(data, size);
 }
 
 /**
 	Calculate 64-bit hash from string reference.
 */
 inline hash64 calc64(StringRef const& ref) {
-	return hash::calc64(ref.data, ref.size);
+	return hash::calc_generic<hash64>(ref.data, ref.size);
 }
 
 /** @} */ // end of doc-group hash
