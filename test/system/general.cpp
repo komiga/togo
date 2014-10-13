cc4bf47295664b54903bd4a27343b52ae1fe7735
Coranna Howard <me@komiga.com>
2014-10-13 00:32:31 -0700

    test/system/general: updated exec_dir() test; tidy.

--- test/system/general.cpp
+++ test/system/general.cpp
@@ -1,15 +1,18 @@
 
 #include <togo/assert.hpp>
 #include <togo/log.hpp>
+#include <togo/string.hpp>
 #include <togo/system.hpp>
 
 using namespace togo;
 
 signed main() {
 	TOGO_LOGF("num_cores = %u\n", system::num_cores());
-	TOGO_LOG("Sleeping for 5 seconds\n");
-	TOGO_LOGF("exec_dir: %s\n", system::exec_dir());
+	StringRef exec_dir = system::exec_dir();
+	TOGO_LOGF("exec_dir: %.*s\n", exec_dir.size, exec_dir.data);
 	TOGO_ASSERTE(system::secs_since_epoch() > 1407135980u);
+
+	TOGO_LOG("\n");
 	float const start = system::time_monotonic();
 	TOGO_LOGF("start       : time_monotonic() = %.6f\n", start);
 	system::sleep_ms(1000u);
@@ -20,5 +23,6 @@ signed main() {
 	TOGO_LOGF("10ms delay  : time_monotonic() = %.6f\n", system::time_monotonic() - start);
 	system::sleep_ms(1u);
 	TOGO_LOGF("1ms delay   : time_monotonic() = %.6f\n", system::time_monotonic() - start);
+
 	return 0;
 }
