d99fd364ca4a682894177615947a7b755bc043e7
Coranna Howard <me@komiga.com>
2014-10-13 00:33:46 -0700

    test/system/general: added working directory interface tests.

--- test/system/general.cpp
+++ test/system/general.cpp
@@ -1,6 +1,7 @@
 
 #include <togo/assert.hpp>
 #include <togo/log.hpp>
+#include <togo/fixed_array.hpp>
 #include <togo/string.hpp>
 #include <togo/system.hpp>
 
@@ -11,6 +12,18 @@ signed main() {
 	StringRef exec_dir = system::exec_dir();
 	TOGO_LOGF("exec_dir: %.*s\n", exec_dir.size, exec_dir.data);
 	TOGO_ASSERTE(system::secs_since_epoch() > 1407135980u);
+
+	FixedArray<char, 256> wd_init{};
+	unsigned const wd_init_size = system::working_dir(wd_init);
+	TOGO_LOGF("wd_init: '%.*s'\n", string::size(wd_init), fixed_array::begin(wd_init));
+	TOGO_ASSERTE(wd_init_size > 0 && wd_init_size == fixed_array::size(wd_init));
+
+	system::change_working_dir("/");
+	FixedArray<char, 256> wd_root{};
+	unsigned const wd_root_size = system::working_dir(wd_root);
+	TOGO_LOGF("wd_root: '%.*s'\n", string::size(wd_root), fixed_array::begin(wd_root));
+	TOGO_ASSERTE(wd_root_size > 0 && wd_root_size == fixed_array::size(wd_root));
+	TOGO_ASSERTE(string::compare_equal(wd_root, "/"));
 
 	TOGO_LOG("\n");
 	float const start = system::time_monotonic();
