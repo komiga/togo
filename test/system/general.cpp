8f32598d52025d883384431b10acde79385afb00
Coranna Howard <me@komiga.com>
2014-10-15 20:06:54 -0700

    test/system/general: removed filesystem module tests.

--- test/system/general.cpp
+++ test/system/general.cpp
@@ -1,7 +1,6 @@
 
 #include <togo/assert.hpp>
 #include <togo/log.hpp>
-#include <togo/fixed_array.hpp>
 #include <togo/string.hpp>
 #include <togo/system.hpp>
 
@@ -9,10 +8,9 @@ using namespace togo;
 
 signed main() {
 	TOGO_LOGF("num_cores = %u\n", system::num_cores());
-	StringRef exec_dir = system::exec_dir();
-	TOGO_LOGF("exec_dir: '%.*s'\n", exec_dir.size, exec_dir.data);
 	TOGO_ASSERTE(system::secs_since_epoch() > 1407135980u);
 
+	// Environment variables
 	StringRef const env_path = system::environment_variable("PATH");
 	TOGO_LOGF("PATH = '%.*s'\n", env_path.size, env_path.data);
 
@@ -32,57 +30,7 @@ signed main() {
 	env_test_value = system::environment_variable(env_test_name);
 	TOGO_ASSERTE(!env_test_value.valid());
 
-	FixedArray<char, 256> wd_init{};
-	unsigned const wd_init_size = system::working_dir(wd_init);
-	TOGO_LOGF("wd_init: '%.*s'\n", string::size(wd_init), fixed_array::begin(wd_init));
-	TOGO_ASSERTE(wd_init_size > 0 && wd_init_size == fixed_array::size(wd_init));
-
-	TOGO_ASSERTE(system::set_working_dir("/"));
-	FixedArray<char, 256> wd_root{};
-	unsigned const wd_root_size = system::working_dir(wd_root);
-	TOGO_LOGF("wd_root: '%.*s'\n", string::size(wd_root), fixed_array::begin(wd_root));
-	TOGO_ASSERTE(wd_root_size > 0 && wd_root_size == fixed_array::size(wd_root));
-	TOGO_ASSERTE(string::compare_equal(wd_root, "/"));
-
-	TOGO_ASSERTE(system::set_working_dir(exec_dir));
-
-	TOGO_ASSERTE(!system::is_file("/non/existent/file"));
-	TOGO_ASSERTE(!system::is_file("."));
-	#if defined(TOGO_PLATFORM_LINUX)
-		TOGO_ASSERTE(system::is_file("general.elf"));
-	#elif defined(TOGO_PLATFORM_WINDOWS)
-		TOGO_ASSERTE(system::is_file("general.exe"));
-	#endif
-
-	TOGO_ASSERTE(!system::is_directory("/non/existent/directory"));
-	TOGO_ASSERTE( system::is_directory("."));
-	TOGO_ASSERTE( system::is_directory(".."));
-
-	#define TEST_DIR "test_dir"
-	#define TEST_FILE TEST_DIR "/test_file"
-
-	// Directory creation
-	TOGO_ASSERTE(!system::is_directory(TEST_DIR));
-	TOGO_ASSERTE(system::create_directory(TEST_DIR));
-	TOGO_ASSERTE(system::is_directory(TEST_DIR));
-	TOGO_ASSERTE(!system::create_directory(TEST_DIR));
-
-	// File creation
-	TOGO_ASSERTE(!system::is_file(TEST_FILE));
-	TOGO_ASSERTE(system::create_file(TEST_FILE));
-	TOGO_ASSERTE(system::is_file(TEST_FILE));
-	TOGO_ASSERTE(!system::create_file(TEST_FILE));
-
-	// File removal
-	TOGO_ASSERTE(system::remove_file(TEST_FILE));
-	TOGO_ASSERTE(!system::is_file(TEST_FILE));
-	TOGO_ASSERTE(!system::remove_file(TEST_FILE));
-
-	// Directory removal
-	TOGO_ASSERTE(system::remove_directory(TEST_DIR));
-	TOGO_ASSERTE(!system::is_directory(TEST_DIR));
-	TOGO_ASSERTE(!system::remove_directory(TEST_DIR));
-
+	// Timers
 	TOGO_LOG("\n");
 	float const start = system::time_monotonic();
 	system::sleep_ms(1000u);
