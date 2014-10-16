c5a1873ab0980a417d997fde7fe27bd5200a6259
Coranna Howard <me@komiga.com>
2014-10-16 15:45:25 -0700

    test/utility/args: added command options; tidy.

--- test/utility/args.cpp
+++ test/utility/args.cpp
@@ -14,32 +14,44 @@ using namespace togo;
 signed main(signed argc, char* argv[]) {
 	memory_init();
 
-	KVS options;
-	KVS command;
-	if (!parse_args(options, command, argc, argv)) {
+	KVS k_options;
+	KVS k_command_options;
+	KVS k_command;
+	if (!parse_args(k_options, k_command_options, k_command, argc, argv)) {
 		TOGO_LOG("#### no command\n");
 	}
 
 	{
 		MemoryStream out_stream{memory::default_allocator(), 1024};
-		TOGO_ASSERTE(kvs::write(options, out_stream));
+		TOGO_ASSERTE(kvs::write(k_options, out_stream));
 		TOGO_LOGF(
 			"#### options (%u): `%.*s`: <%.*s>\n",
 			static_cast<unsigned>(array::size(out_stream.buffer())),
-			kvs::name_size(options), kvs::name(options),
+			kvs::name_size(k_options), kvs::name(k_options),
 			static_cast<unsigned>(array::size(out_stream.buffer())),
 			array::begin(out_stream.buffer())
 		);
 		out_stream.clear();
 
-		TOGO_ASSERTE(kvs::write(command, out_stream));
+		TOGO_ASSERTE(kvs::write(k_command_options, out_stream));
 		TOGO_LOGF(
-			"#### command (%u): `%.*s` <%.*s>\n",
+			"#### command options (%u): `%.*s` <%.*s>\n",
 			static_cast<unsigned>(array::size(out_stream.buffer())),
-			kvs::name_size(command), kvs::name(command),
+			kvs::name_size(k_command_options), kvs::name(k_command_options),
 			static_cast<unsigned>(array::size(out_stream.buffer())),
 			array::begin(out_stream.buffer())
 		);
+		out_stream.clear();
+
+		TOGO_ASSERTE(kvs::write(k_command, out_stream));
+		TOGO_LOGF(
+			"#### command (%u): `%.*s` <%.*s>\n",
+			static_cast<unsigned>(array::size(out_stream.buffer())),
+			kvs::name_size(k_command), kvs::name(k_command),
+			static_cast<unsigned>(array::size(out_stream.buffer())),
+			array::begin(out_stream.buffer())
+		);
+		out_stream.clear();
 	}
 
 	return 0;
