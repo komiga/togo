cd958c2bb6733db56947a3070b427e44509437a7
Coranna Howard <me@komiga.com>
2014-12-15 02:15:15 -0500

    gfx/types: #include tidy.

--- src/togo/gfx/types.hpp
+++ src/togo/gfx/types.hpp
@@ -12,10 +12,10 @@
 
 #include <togo/config.hpp>
 #include <togo/types.hpp>
-#include <togo/collection/types.hpp>
-#include <togo/hash/hash.hpp>
 #include <togo/utility/traits.hpp>
 #include <togo/utility/utility.hpp>
+#include <togo/collection/types.hpp>
+#include <togo/hash/hash.hpp>
 #include <togo/serialization/types.hpp>
 #include <togo/resource/types.hpp>
 
