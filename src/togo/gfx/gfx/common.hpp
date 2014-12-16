b6adb436ea9a1415ce8447440fbeedac26067215
Coranna Howard <me@komiga.com>
2014-12-16 01:59:06 -0500

    gfx/gfx/common: use TOGO_GLCE() immediately after glewInit().¹
    1: Ugh, GLEW. glewInit() makes an invalid call on my current build,
    so we'll just always check to keep any from sneaking up on the next
    error check.

--- src/togo/gfx/gfx/common.hpp
+++ src/togo/gfx/gfx/common.hpp
@@ -7,6 +7,7 @@
 
 #include <togo/config.hpp>
 #include <togo/error/assert.hpp>
+#include <togo/gfx/gfx/opengl_common.hpp>
 
 #include <GL/glew.h>
 
@@ -26,7 +27,12 @@ inline void glew_init() {
 	if (!_gfx_globals.glew_initialized) {
 		glewExperimental = GL_TRUE;
 		GLenum const err = glewInit();
-		TOGO_ASSERTF(err == GLEW_OK, "failed to initialize GLEW: %s\n", glewGetErrorString(err));
+		TOGO_ASSERTF(
+			err == GLEW_OK,
+			"failed to initialize GLEW: %s\n",
+			glewGetErrorString(err)
+		);
+		TOGO_GLCE();
 		_gfx_globals.glew_initialized = true;
 	}
 }
