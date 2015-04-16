18084204b1cd0ade3ac97b2fd0474e59b8647cc0
Coranna Howard <me@komiga.com>
2015-04-16 04:29:44 -0400

    tool/res_build/generator_compiler/gc_clear: store render target index instead of name hash.

--- tool/res_build/src/togo/tool_res_build/generator_compiler/gc_clear.cpp
+++ tool/res_build/src/togo/tool_res_build/generator_compiler/gc_clear.cpp
@@ -25,21 +25,55 @@ namespace gc_clear {
 static bool write(
 	GeneratorCompiler& /*gen_compiler*/,
 	BinaryOutputSerializer& ser,
-	gfx::RenderConfig const& /*render_config*/,
+	gfx::RenderConfig const& render_config,
 	gfx::GeneratorUnit const& unit
 ) {
 	auto* const k_root = static_cast<KVS const*>(unit.data);
 
 	// Fetch and validate structure
-	auto* const k_rt = kvs::find(*k_root, "rt");
+	auto* const k_rt = kvs::find(*k_root, "render_target");
 	if (k_rt && (!kvs::is_type(*k_rt, KVSType::string) || !kvs::string_size(*k_rt))) {
-		TOGO_LOG_ERROR("malformed generator: clear: 'rt' is either not a string or is empty\n");
+		TOGO_LOG_ERROR(
+			"malformed generator: clear: "
+			"'render_target' is either not a string or is empty\n"
+		);
 		return false;
 	}
 
-	// TODO: Validate RT?
-	hash32 const rt = k_rt ? hash::calc32(kvs::string_ref(*k_rt)) : "back_buffer"_hash32;
-	ser % rt;
+	// Generator data
+	u32 rt_index = ~u32{0};
+
+	{// Read render_target
+	StringRef const rt_name = k_rt ? kvs::string_ref(*k_rt) : "back_buffer";
+	hash32 const rt_name_hash = hash::calc32(rt_name);
+	if (rt_name_hash != "back_buffer"_hash32) {
+		rt_index = 0;
+		for (auto const& resource : render_config.shared_resources) {
+			if (resource.name_hash == rt_name_hash) {
+				if (resource.type() == gfx::RenderConfigResource::TYPE_RENDER_TARGET) {
+					goto l_found;
+				} else {
+					TOGO_LOG_ERRORF(
+						"malformed generator: clear: "
+						"render_target '%.*s' is not a render target\n",
+						rt_name.size, rt_name.data
+					);
+				}
+			} else if (resource.type() == gfx::RenderConfigResource::TYPE_RENDER_TARGET) {
+				++rt_index;
+			}
+		}
+		TOGO_LOG_ERRORF(
+			"malformed generator: clear: "
+			"render_target '%.*s' not found\n",
+			rt_name.size, rt_name.data
+		);
+
+		l_found:
+		(void)0;
+	}}
+
+	ser % rt_index;
 	return true;
 }
 
