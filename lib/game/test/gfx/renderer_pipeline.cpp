0e1a3adde7113d937773ee2e1c3cdfe290daadde
Coranna Howard <me@komiga.com>
2015-04-18 03:43:15 -0400

    lib/game/test/gfx/renderer_pipeline: use func_init for test_proxy generator; tidy.

--- lib/game/test/gfx/renderer_pipeline.cpp
+++ lib/game/test/gfx/renderer_pipeline.cpp
@@ -3,21 +3,16 @@
 #include <togo/core/utility/utility.hpp>
 #include <togo/core/math/math.hpp>
 #include <togo/core/log/log.hpp>
-#include <togo/game/entity/types.hpp>
 #include <togo/game/entity/entity_manager.hpp>
-#include <togo/game/world/types.hpp>
 #include <togo/game/world/world_manager.hpp>
-#include <togo/game/gfx/types.hpp>
 #include <togo/game/gfx/gfx.hpp>
 #include <togo/game/gfx/display.hpp>
 #include <togo/game/gfx/command.hpp>
 #include <togo/game/gfx/render_node.hpp>
 #include <togo/game/gfx/renderer.hpp>
-#include <togo/game/gfx/renderer/types.hpp>
 #include <togo/game/gfx/renderer/private.hpp>
 #include <togo/game/gfx/renderer/opengl.hpp>
 #include <togo/game/input/input.hpp>
-#include <togo/game/resource/types.hpp>
 #include <togo/game/resource/resource_handler.hpp>
 #include <togo/game/resource/resource_manager.hpp>
 #include <togo/game/app/app.hpp>
@@ -48,11 +43,46 @@ static Vertex const triangle_vertices[]{
 };
 
 struct GenTestProxyData {
-	gfx::Renderer* renderer;
 	gfx::BufferID buffer;
 	gfx::BufferBindingID binding;
 	gfx::ShaderID shader;
 };
+
+void gen_test_proxy_init(
+	gfx::GeneratorDef& def,
+	gfx::Renderer* renderer
+) {
+	if (def.data) {
+		return;
+	}
+	auto& app = app::instance();
+	auto* data = TOGO_CONSTRUCT_DEFAULT(
+		memory::default_allocator(), GenTestProxyData
+	);
+
+	data->buffer = gfx::renderer::create_buffer(
+		renderer, sizeof(triangle_vertices), triangle_vertices
+	);
+	data->binding = gfx::renderer::create_buffer_binding(
+		renderer, array_extent(triangle_vertices), 0, {},
+		{{data->buffer, &triangle_vformat, 0}}
+	);
+	data->shader = resource::load_shader(
+		app.resource_manager,
+		"test/gfx/generic"_resource_name
+	);
+	def.data = data;
+}
+
+void gen_test_proxy_destroy(
+	gfx::GeneratorDef const& def,
+	gfx::Renderer* renderer
+) {
+	auto* data = static_cast<GenTestProxyData*>(def.data);
+	gfx::renderer::destroy_buffer_binding(renderer, data->binding);
+	gfx::renderer::destroy_buffer(renderer, data->buffer);
+	TOGO_DESTROY(memory::default_allocator(), data);
+}
 
 void gen_test_proxy_exec(
 	gfx::GeneratorUnit const& unit,
@@ -84,46 +114,6 @@ void gen_test_proxy_read(
 	unit.data = def.data;
 }
 
-void gen_test_proxy_destroy(
-	gfx::GeneratorDef const& def,
-	gfx::Renderer* renderer
-) {
-	auto* data = static_cast<GenTestProxyData*>(def.data);
-	gfx::renderer::destroy_buffer_binding(renderer, data->binding);
-	gfx::renderer::destroy_buffer(renderer, data->buffer);
-	TOGO_DESTROY(memory::default_allocator(), data);
-}
-
-void gen_test_proxy_register() {
-	auto& app = app::instance();
-	auto* data = TOGO_CONSTRUCT_DEFAULT(
-		memory::default_allocator(), GenTestProxyData
-	);
-
-	data->renderer = app.renderer;
-	data->buffer = gfx::renderer::create_buffer(
-		app.renderer, sizeof(triangle_vertices), triangle_vertices
-	);
-	data->binding = gfx::renderer::create_buffer_binding(
-		app.renderer, array_extent(triangle_vertices), 0, {},
-		{{data->buffer, &triangle_vformat, 0}}
-	);
-	data->shader = resource::load_shader(
-		app.resource_manager,
-		"test/gfx/generic"_resource_name
-	);
-
-	gfx::renderer::register_generator_def(
-		app.renderer,
-		gfx::GeneratorDef{
-			"test_proxy"_generator_name,
-			data,
-			gen_test_proxy_destroy,
-			gen_test_proxy_read
-		}
-	);
-}
-
 struct TestAppData {
 	WorldID world;
 	EntityID camera;
@@ -137,7 +127,16 @@ void TestAppModel::init(TestApp& app) {
 	resource_manager::add_package(app.resource_manager, "test_data");
 	gfx::display::set_swap_mode(app.display, gfx::DisplaySwapMode::wait_refresh);
 
-	gen_test_proxy_register();
+	gfx::renderer::register_generator_def(
+		app.renderer,
+		gfx::GeneratorDef{
+			"test_proxy"_generator_name,
+			nullptr,
+			gen_test_proxy_init,
+			gen_test_proxy_destroy,
+			gen_test_proxy_read
+		}
+	);
 	auto* render_config = resource::load_render_config(
 		app.resource_manager,
 		"test/gfx/pipeline"_resource_name
