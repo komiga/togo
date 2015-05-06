81335ef70410ca88f8132159804e0edc97a8fdd4
Coranna Howard <me@komiga.com>
2015-05-05 20:30:05 -0400

    lib/image/test/pixmap/general: tidy.

--- lib/image/test/pixmap/general.cpp
+++ lib/image/test/pixmap/general.cpp
@@ -11,28 +11,24 @@ using namespace togo;
 signed main() {
 	memory_init();
 
-	Color c_test1{0xAA, 0xBB, 0xCC, 0xDD};
-	Color c_test2{0xDD, 0xCC, 0xBB, 0xAA};
+	Color c_init{0x01, 0x02, 0x03, 0x04};
+	Color c_fill1{0xAA, 0xBB, 0xCC, 0xDD};
+	auto cp_init = c_init.packed();
+	auto cp_fill1 = c_fill1.packed();
 
 	Pixmap p{PixelFormatID::rgba8};
-	pixmap::resize(p, UVec2{1, 1});
+	pixmap::resize(p, UVec2{1, 1}, c_init);
 	TOGO_ASSERTE(p.data && p.data_size == p.data_capacity && p.data_size == 1*1 * 4);
 	auto data = reinterpret_cast<u32*>(p.data);
-	data[0] = c_test1.packed();
+	TOGO_ASSERTE(data[0] == cp_init);
 
-	pixmap::resize(p, UVec2{4, 4}, c_test2);
+	pixmap::resize(p, UVec2{4, 4}, c_fill1);
 	TOGO_ASSERTE(p.data && p.data_size == p.data_capacity && p.data_size == 4*4 * 4);
 	data = reinterpret_cast<u32*>(p.data);
-	TOGO_ASSERTE(data[0] == c_test1.packed());
-
+	TOGO_ASSERTE(data[0] == cp_init);
 	{auto end = pointer_add(data, p.data_size);
-	auto c_test2_packed = c_test2.packed();
 	for (auto check = data + 1; check != end; ++check) {
-		TOGO_ASSERTF(
-			*check == c_test2_packed,
-			"%08x != %08x",
-			*check, c_test2_packed
-		);
+		TOGO_ASSERTE(*check == cp_fill1);
 	}}
 
 	return 0;
