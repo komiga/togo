cb5a0328e51cc28ff4d2f914a1d273a771fb4457
Coranna Howard <me@komiga.com>
2015-05-05 20:31:35 -0400

    lib/image/test/pixmap/general: added copy() and blit() tests.

--- lib/image/test/pixmap/general.cpp
+++ lib/image/test/pixmap/general.cpp
@@ -6,6 +6,8 @@
 
 #include <togo/support/test.hpp>
 
+#include <cstring>
+
 using namespace togo;
 
 signed main() {
@@ -13,8 +15,10 @@ signed main() {
 
 	Color c_init{0x01, 0x02, 0x03, 0x04};
 	Color c_fill1{0xAA, 0xBB, 0xCC, 0xDD};
+	Color c_fill2{0xDD, 0xCC, 0xBB, 0xAA};
 	auto cp_init = c_init.packed();
 	auto cp_fill1 = c_fill1.packed();
+	// auto cp_fill2 = c_fill2.packed();
 
 	Pixmap p{PixelFormatID::rgba8};
 	pixmap::resize(p, UVec2{1, 1}, c_init);
@@ -30,6 +34,21 @@ signed main() {
 	for (auto check = data + 1; check != end; ++check) {
 		TOGO_ASSERTE(*check == cp_fill1);
 	}}
+
+	{Pixmap copy{p.format};
+	pixmap::copy(copy, p);
+	TOGO_ASSERTE(
+		copy.data_size == p.data_size &&
+		std::memcmp(copy.data, p.data, p.data_size) == 0
+	);}
+
+	{Pixmap blit{p.format};
+	pixmap::resize(blit, UVec2{4, 4}, c_fill2);
+	pixmap::blit(blit, p, UVec2{0, 0});
+	TOGO_ASSERTE(
+		blit.data_size == p.data_size &&
+		std::memcmp(blit.data, p.data, p.data_size) == 0
+	);}
 
 	return 0;
 }
