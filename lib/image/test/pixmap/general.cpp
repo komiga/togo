298f471ffebc4cc23c28cb288fd7bc65a6b63550
Coranna Howard <me@komiga.com>
2015-05-31 00:04:04 -0400

    lib/image/test/pixmap/general: URC.

--- lib/image/test/pixmap/general.cpp
+++ lib/image/test/pixmap/general.cpp
@@ -13,21 +13,23 @@ using namespace togo;
 signed main() {
 	memory_init();
 
+	PixelFormat format{PixelFormatID::rgba};
+	auto ps = pixel_format::pixel_size(format);
 	Color c_init{0x01, 0x02, 0x03, 0x04};
 	Color c_fill1{0xAA, 0xBB, 0xCC, 0xDD};
 	Color c_fill2{0xDD, 0xCC, 0xBB, 0xAA};
-	auto cp_init = c_init.packed();
-	auto cp_fill1 = c_fill1.packed();
-	// auto cp_fill2 = c_fill2.packed();
+	auto cp_init = pixel_format::pack(format, c_init);
+	auto cp_fill1 = pixel_format::pack(format, c_fill1);
+	// auto cp_fill2 = pixel_format::pack(format, c_fill2);
 
-	Pixmap p{PixelFormatID::rgba8};
+	Pixmap p{format};
 	pixmap::resize(p, UVec2{1, 1}, c_init);
-	TOGO_ASSERTE(p.data && p.data_size == p.data_capacity && p.data_size == 1*1 * 4);
+	TOGO_ASSERTE(p.data && p.data_size == p.data_capacity && p.data_size == 1*1 * ps);
 	auto data = reinterpret_cast<u32*>(p.data);
 	TOGO_ASSERTE(data[0] == cp_init);
 
 	pixmap::resize(p, UVec2{4, 4}, c_fill1);
-	TOGO_ASSERTE(p.data && p.data_size == p.data_capacity && p.data_size == 4*4 * 4);
+	TOGO_ASSERTE(p.data && p.data_size == p.data_capacity && p.data_size == 4*4 * ps);
 	data = reinterpret_cast<u32*>(p.data);
 	TOGO_ASSERTE(data[0] == cp_init);
 	{auto end = pointer_add(data, p.data_size);
@@ -35,14 +37,14 @@ signed main() {
 		TOGO_ASSERTE(*check == cp_fill1);
 	}}
 
-	{Pixmap copy{p.format};
+	{Pixmap copy{PixelFormatID::xrgb};
 	pixmap::copy(copy, p);
 	TOGO_ASSERTE(
 		copy.data_size == p.data_size &&
 		std::memcmp(copy.data, p.data, p.data_size) == 0
 	);}
 
-	{Pixmap blit{p.format};
+	{Pixmap blit{format};
 	pixmap::resize(blit, UVec2{4, 4}, c_fill2);
 	pixmap::blit(blit, p, UVec2{0, 0});
 	TOGO_ASSERTE(
