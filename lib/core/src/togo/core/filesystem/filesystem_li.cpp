a29ff692d7cd5c9e692ef09e3b511d94a88ef4ae
Coranna Howard <me@komiga.com>
2016-01-04 21:43:21 -0500

    lib/core/filesystem/filesystem_li: implemented path_dir().

--- lib/core/src/togo/core/filesystem/filesystem_li.cpp
+++ lib/core/src/togo/core/filesystem/filesystem_li.cpp
@@ -23,6 +23,12 @@ TOGO_LI_FUNC_DEF(destroy) {
 
 namespace filesystem {
 
+TOGO_LI_FUNC_DEF(path_dir) {
+	auto path = lua::get_string(L, 1);
+	lua::push_value(L, filesystem::path_dir(path));
+	return 1;
+}
+
 TOGO_LI_FUNC_DEF(working_dir) {
 	FixedArray<char, 256> path{};
 	filesystem::working_dir(path);
@@ -135,6 +141,7 @@ TOGO_LI_FUNC_DEF(iterate_dir) {
 } // namespace filesystem
 
 static luaL_reg const li_funcs[]{
+	TOGO_LI_FUNC_REF(filesystem, path_dir)
 	TOGO_LI_FUNC_REF(filesystem, working_dir)
 	TOGO_LI_FUNC_REF(filesystem, set_working_dir)
 	TOGO_LI_FUNC_REF(filesystem, is_file)
