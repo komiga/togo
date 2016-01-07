7c28de0a87f470308b20ddd947a35b6ada2334c1
Coranna Howard <me@komiga.com>
2016-01-07 03:47:02 -0500

    lib/core/test/lua/utility.lua: added parse_args() test.

--- lib/core/test/lua/utility.lua
+++ lib/core/test/lua/utility.lua
@@ -13,3 +13,42 @@ local U = require "togo.utility"
 U.log("util test")
 
 print_preload()
+
+do
+	local command_parsed, opt, cmd_opt, cmd = U.parse_args({
+		"exec",
+		"-a",
+		"--b=c",
+
+		"x",
+		"-y",
+		"1",
+		"2",
+		"-z",
+		"3",
+	})
+	U.assert(command_parsed)
+
+	local t, i
+	local function check(name, value)
+		local p = t[i]
+		U.assert(p.name == name and p.value == value)
+		i = i + 1
+	end
+
+	t = opt; i = 1
+	U.assert(t.name == "exec")
+	check("-a", true)
+	check("--b", "c")
+
+	t = cmd_opt; i = 1
+	U.assert(t.name == "")
+	check("-y", true)
+
+	t = cmd; i = 1
+	U.assert(t.name == "x")
+	check("", "1")
+	check("", "2")
+	check("-z", true)
+	check("", "3")
+end
