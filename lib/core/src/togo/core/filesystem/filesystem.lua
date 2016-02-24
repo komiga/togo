60ccb78575958d8553f9441dde2050067fa546a0
Coranna Howard <me@komiga.com>
2016-02-24 11:10:37 -0500

    lib/core/filesystem/filesystem.lua: added working_dir_scope().

--- lib/core/src/togo/core/filesystem/filesystem.lua
+++ lib/core/src/togo/core/filesystem/filesystem.lua
@@ -5,6 +5,17 @@ local M = U.module(...)
 
 M.debug = false
 
+function M.working_dir_scope(path, f)
+	U.type_assert(path, "string")
+	U.type_assert(f, "function")
+
+	local previous_path = M.working_dir()
+	U.assert(previous_path ~= "")
+	U.assert(M.set_working_dir(path))
+	f(previous_path, path)
+	U.assert(M.set_working_dir(previous_path))
+end
+
 return M
 
 )"__RAW_STRING__"
